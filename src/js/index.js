09a99e49fb1e0373c1bae4d3e4dbeeef386e667d
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-08-13 13:59:23 -0300

    add: função para alterar as cores

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const btnEscuro = document.getElementById('btnEscuro');
+const btnClaro = document.getElementById('btnClaro');
+
+btnEscuro.addEventListener('click', checkedRadio);
+btnClaro.addEventListener('click', checkedRadio);
+
+const cssTheme = document.getElementById('color-theme');
+
+function lightToDark() {
+    cssTheme.href = "./src/css/color_dark.css";
+}
+
+function darkToLight() {    
+    cssTheme.href = "./src/css/color_light.css";
+}
+
+function checkedRadio() {
+  if (btnClaro.checked) {
+    darkToLight();
+  } else  {
+    lightToDark();
+  }
+}
+
+function checkUserPreference() {
+  const prefersLightTheme = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+  if (prefersLightTheme) {
+    btnEscuro.checked = false;
+    btnClaro.checked = true;
+  } else {
+    btnClaro.checked = false;
+    btnEscuro.checked = true;
+  }
+  checkedRadio() 
+}
+
+checkUserPreference();
