6a8a7b71d2e4e4df326e1c0dba9b49b1a601a0da
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-11-15 17:05:26 -0300

    update: class usuario, servicos e agendamentos.

--- java/iformal/src/main/java/com/example/iformal/model/Agendamentos.java
+++ java/iformal/src/main/java/com/example/iformal/model/Agendamentos.java
@@ -1,4 +1,52 @@
 package com.example.iformal.model;
 public class Agendamentos {
+    private int id;
+    private Usuario usuario;
+    private Servicos servico;
+    private String data_servico;
+    private String observacao;
+    private float preco;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public Servicos getServico() {
+        return servico;
+    }
+    public void setServico(Servicos servico) {
+        this.servico = servico;
+    }
+    public String getData_servico() {
+        return data_servico;
+    }
+    public void setData_servico(String data_servico) {
+        this.data_servico = data_servico;
+    }
+    public String getObservacao() {
+        return observacao;
+    }
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    public float getPreco() {
+        return preco;
+    }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    @Override
+    public String toString() {
+        return "Agendamentos [id=" + id + ", usuario=" + usuario + ", servico=" + servico + ", data_servico="
+                + data_servico + ", observacao=" + observacao + ", preco=" + preco + "]";
+    }
+
 
 }
--- java/iformal/src/main/java/com/example/iformal/model/Servicos.java
+++ java/iformal/src/main/java/com/example/iformal/model/Servicos.java
@@ -1,4 +1,38 @@
 package com.example.iformal.model;
+
 public class Servicos {
+
+    private int id;
+    private String nome;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Servicos [id=" + id + ", nome=" + nome + ", descricao=" + descricao + "]";
+    }
 
 }
