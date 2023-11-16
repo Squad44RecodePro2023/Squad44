a3149665e47a108e539d41a3743684bb3959f6c1
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-11-15 23:22:01 -0300

    Adicionadas classes Repository para Usuario, Servicos e Agendamentos

---
+++ java/iformal/src/main/java/com/example/iformal/repository/AgendamentosRepository.java
@@ -0,0 +1,9 @@
+package com.example.iformal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.iformal.model.Agendamentos;
+
+public interface AgendamentosRepository extends JpaRepository<Agendamentos, Integer> {
+    // TODO
+};
---
+++ java/iformal/src/main/java/com/example/iformal/repository/ServicosRepository.java
@@ -0,0 +1,9 @@
+package com.example.iformal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.iformal.model.Servicos;
+
+public interface ServicosRepository extends JpaRepository <Servicos, Integer> {
+    //TODO
+}
