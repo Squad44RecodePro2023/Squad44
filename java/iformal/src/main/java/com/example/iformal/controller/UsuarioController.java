318ee28ea3c0e3e2f986e14758fbc8de0c713bef
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-11-17 20:34:29 -0300

    refactor: remove imports não utilizados
    - Limpeza de imports não utilizados nos arquivos do projeto

--- java/iformal/src/main/java/com/example/iformal/controller/UsuarioController.java
+++ java/iformal/src/main/java/com/example/iformal/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.example.iformal.controller;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.iformal.model.Usuario;
