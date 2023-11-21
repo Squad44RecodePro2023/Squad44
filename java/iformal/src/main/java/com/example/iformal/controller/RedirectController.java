a9c67aafc1dff55c135136da3845d8e215da8ba5
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-11-20 22:00:52 -0300

    removido imports desnecessários

--- java/iformal/src/main/java/com/example/iformal/controller/RedirectController.java
+++ java/iformal/src/main/java/com/example/iformal/controller/RedirectController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.iformal.model.Usuario;
 import com.example.iformal.repository.UsuarioRepository;
